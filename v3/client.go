7717fe91e4231c152a061efb9cd0182860a076b8
LinkinStar <linkinstar@foxmail.com>
2019-04-24 17:20:44 +0800

    限制用户发送消息频率

--- v3/client.go
+++ v3/client.go
@@ -2,6 +2,8 @@ package v3
 
 import (
 	"github.com/gorilla/websocket"
+	"go.uber.org/zap"
+	"time"
 )
 
 // 客户端
@@ -12,6 +14,7 @@ type Client struct {
 
 // 读取消息
 func (c *Client) ReadMessage() {
+	preMessageTime := int64(0)
 	for {
 		// 接收消息
 		message := &Message{}
@@ -20,6 +23,14 @@ func (c *Client) ReadMessage() {
 			chatRoom.unregister <- c
 			return
 		}
+
+		// 限制用户发送消息频率，每1秒只能发送一条消息
+		curMessageTime := time.Now().Unix()
+		if curMessageTime-preMessageTime < 1 {
+			zap.S().Warn("1秒内无法再次发送")
+			continue
+		}
+		preMessageTime = curMessageTime
 
 		chatRoom.send <- *message
 	}
